372b1392d94d23833d50b79963c4a607b3c067ed
Yash Aggarwal <yash0609@gmail.com>
2012-03-12 16:15:55 -0500

    input starting title number. this will be automated soon though

--- dvdrip.rb
+++ dvdrip.rb
@@ -14,12 +14,15 @@ directory_name = gets.to_s.gsub("\n", "")
 print "Starting Lecture? (1 for beginning) "
 beginning_lecture = gets.to_i
 
+print "Starting Title? "
+starting_title = gets.to_i
+
 new_dir_command = "mkdir #{directory_name}"
 system new_dir_command
 
 for lecture_number in (beginning_lecture..number_of_videos)
 	videos_per_disc = 6
-	track_number_to_rip = ((lecture_number % videos_per_disc == 0) ? videos_per_disc : lecture_number % videos_per_disc) + 1
+	track_number_to_rip = ((lecture_number % videos_per_disc == 0) ? videos_per_disc : lecture_number % videos_per_disc) + starting_title - 1
 	rip_command = "./HandBrakeCLI -i #{DVD_DRIVE_LOCATION} --preset=\"Normal\" -t #{track_number_to_rip} -o #{directory_name}" + "/#{lecture_number}.mp4"
 	system rip_command
 	if((lecture_number % videos_per_disc == 0) && lecture_number != number_of_videos)
