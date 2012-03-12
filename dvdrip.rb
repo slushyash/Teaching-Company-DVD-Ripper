c494b1aee89c1c983ccd4194171275090a9bf460
Yash Aggarwal <yash0609@gmail.com>
2012-03-11 22:28:41 -0500

    First commit

---
+++ dvdrip.rb
@@ -0,0 +1,28 @@
+def sendNotification(message)
+	str = "curl http://api.airgramapp.com/1/send_as_guest \\\n --data-urlencode email='yash0609@gmail.com' \\\n --data-urlencode msg='#{message}'"
+	system(str)
+end
+
+
+print "Number of Parts = "
+number_of_videos = gets.to_i * 12
+
+print "Name of Directory? "
+directory_name = gets.to_s.gsub("\n", "");
+
+new_dir_command = "mkdir #{directory_name}"
+system(new_dir_command)
+
+number_of_videos.times do |i|
+	lecture_number = i + 1
+	videos_per_disc = 6
+	track_number_to_rip = ((lecture_number % videos_per_disc == 0) ? 6 : lecture_number % videos_per_disc) + 1
+	rip_command = "./HandBrakeCLI -i /dev/disk1 --preset=\"Normal\" -t #{track_number_to_rip} --deinterlace=\"fast\" -o #{directory_name}" + "/#{lecture_number}.mp4"
+	system(rip_command);
+	if((lecture_number % videos_per_disc == 0) && lecture_number != number_of_videos)
+		sendNotification("Laptop: New Disc Needed");
+		print "Put in new disc. "
+		`drutil eject`
+		gets
+	end
+end
