9a9f7b97546134df63d438eb0fca870344933a5a
Yash Aggarwal <yash0609@gmail.com>
2012-03-12 10:19:25 -0500

    cleaned up parens and semicolons

--- dvdrip.rb
+++ dvdrip.rb
@@ -15,15 +15,15 @@ print "Starting Lecture? (1 for beginning) "
 beginning_lecture = gets.to_i
 
 new_dir_command = "mkdir #{directory_name}"
-system(new_dir_command)
+system new_dir_command
 
 for lecture_number in (beginning_lecture..number_of_videos)
 	videos_per_disc = 6
-	track_number_to_rip = ((lecture_number % videos_per_disc == 0) ? 6 : lecture_number % videos_per_disc) + 1
+	track_number_to_rip = ((lecture_number % videos_per_disc == 0) ? videos_per_disc : lecture_number % videos_per_disc) + 1
 	rip_command = "./HandBrakeCLI -i #{DVD_DRIVE_LOCATION} --preset=\"Normal\" -t #{track_number_to_rip} --deinterlace=\"fast\" -o #{directory_name}" + "/#{lecture_number}.mp4"
-	system(rip_command);
+	system rip_command
 	if((lecture_number % videos_per_disc == 0) && lecture_number != number_of_videos)
-		sendNotification("Laptop: New Disc Needed");
+		sendNotification "Laptop: New Disc Needed"
 		print "Put in new disc. "
 		`drutil eject`
 		gets
